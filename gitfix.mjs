36a0a4856f976ac38fbeb24931bd63977dba33b0
ddosakura <moyinzi@outlook.com>
2022-04-13 03:02:09 +0000

    build: tool gitfix

---
+++ gitfix.mjs
@@ -0,0 +1,7 @@
+#!/usr/bin/env zx
+
+$`git config --local core.autocrlf false`
+$`git config --local http.proxy ""`
+$`git config --local http.sslcainfo ""`
+$`git config --local http.sslBackend "gnutls"`
+$`git config --local http.sslVerify false`
