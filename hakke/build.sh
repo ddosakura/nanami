f192f7389c193a30fde1e8bba6b8858db0dc47e9
ddosakura <moyinzi@outlook.com>
2022-04-20 07:27:55 +0000

    image: ddosakura/hakke:0.1.1

--- hakke/build.sh
+++ hakke/build.sh
@@ -1,1 +1,1 @@
-docker build -t ddosakura/hakke:0.1.0 -f Dockerfile .
+docker build -t ddosakura/hakke:0.1.1 -f Dockerfile .
--- hakke/push.sh
+++ hakke/push.sh
@@ -1,1 +1,1 @@
-docker push ddosakura/hakke:0.1.0
+docker push ddosakura/hakke:0.1.1
