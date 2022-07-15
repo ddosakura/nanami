3ff3cf22d4f99b720bc123f2bb4458b6b5ea2a87
ddosakura <moyinzi@outlook.com>
2022-07-15 23:17:00 +0800

    image: ddosakura/hakke:0.2.1

--- hakke/version.mjs
+++ hakke/version.mjs
@@ -1,18 +1,20 @@
 #!/usr/bin/env zx
 
 const IMAGE_NAME = 'ddosakura/hakke'
-const IMAGE_VERSION = '0.2.0'
+const IMAGE_VERSION = '0.2.1'
 const BASE_IMAGE = 'ubuntu:20.04'
-const DENO_VERSION = '1.20.5'
-const NODE_VERSION = '16.14.2'
+const DENO_VERSION = '1.23.3'
+const NODE_VERSION = '16.16.0'
+const PNPM_VERSION = '7.5.2'
 // const RUST_VERSION = 'stable'
-const RUST_VERSION = '1.60.0'
+const RUST_VERSION = '1.62.0'
 
 const file = String(await fs.readFile(`./Dockerfile`))
 await fs.writeFile(`./Dockerfile`, file.split('\n').map(line => {
     if (line.startsWith('FROM ')) return `FROM ${BASE_IMAGE}`
     if (line.startsWith('ARG DENO_VERSION=')) return `ARG DENO_VERSION=${DENO_VERSION}`
     if (line.startsWith('ARG NODE_VERSION=')) return `ARG NODE_VERSION=${NODE_VERSION}`
+    if (line.startsWith('ARG PNPM_VERSION=')) return `ARG PNPM_VERSION=pnpm@${NODE_VERSION}`
     if (line.startsWith('ARG RUST_VERSION=')) return `ARG RUST_VERSION=${RUST_VERSION}`
     return line
 }).join('\n'))
